714912ee9c1f363576c7a3aa793ed27064a09f40
Vitor Andrey <vitor.andreylopess@gmail.com>
2023-11-04 14:23:20 -0300

    feat: add docker compose file

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,11 @@
 import fastify from "fastify";
+import { PrismaClient } from "@prisma/client";
 
 export const app = fastify();
+const prisma = new PrismaClient();
+
+app.get("/users", async (req, res) => {
+  const users = await prisma.user.findMany();
+
+  return res.status(200).send(users);
+});
